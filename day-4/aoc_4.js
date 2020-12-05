ac428e031f23ae84c49c065d8510582c4a193e67
niQQe <nickewideving@gmail.com>
2020-12-05 19:07:28 +0100

    day-4

--- day-4/aoc_4.js
+++ day-4/aoc_4.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 
 const input = fs.readFileSync('input.txt', 'utf-8')
 
-const passports = input.split('\n\n').map(pass => pass.replace(/[ ||\n]/g, ',').split`,`)
+const passports = input.split`\n\n`.map(pass => pass.replace(/[ ||\n]/g, ',').split`,`)
 
-const validFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+const requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-const validPassports = passports.reduce((fields, curr) => {
-	const fieldList = curr.reduce((acc, field) => {
-		const [ firstField ] = field.split`:`
-		if (firstField != 'cid') acc.push(field)
-		return acc
-	}, [])
-	if (fieldList.length == validFields.length) fields++
-	return fields
+const validPassports = passports.reduce((passports, row) => {
+	const valid = row.reduce((valid, fieldRow) => {
+		const [field] = fieldRow.split`:`
+		if (requiredFields.includes(field)) valid++
+		return valid
+	}, 0)
+	if (valid == requiredFields.length) passports++
+	return passports
 }, 0)
 
 console.log(validPassports)
