b927b96494c70a6abc51c55a29e8f561379b1d38
niQQe <nickewideving@gmail.com>
2020-12-05 17:14:32 +0100

    day-4

--- day-4/aoc_4.js
+++ day-4/aoc_4.js
@@ -5,12 +5,12 @@ const input = fs.readFileSync('input.txt', 'utf-8')
 const passports = input.split('\n\n').map(pass => pass.replace(/ /g, '\n').replace(/\n/g, ',').split`,`)
 
 const validPasswords = passports.reduce((fields, curr) => {
-	const fieldsArr = curr.reduce((acc, field) => {
+	const fieldList = curr.reduce((acc, field) => {
 		[field] = field.split`:`
 		if (field != 'cid') acc.push(field)
 		return acc
 	}, [])
-	if (fieldsArr.length == 7) fields++
+	if (fieldList.length == 7) fields++
 	return fields
 }, 0)
 
