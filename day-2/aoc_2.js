315f0a104b9b367dc861352a499e664179997849
niQQe <nickewideving@gmail.com>
2020-12-05 21:15:07 +0100

    day-2 part2

--- day-2/aoc_2.js
+++ day-2/aoc_2.js
@@ -10,11 +10,32 @@ const passwordObjects = fs.readFileSync('input.txt', 'utf-8')
 		return acc
 	}, [])
 
-const validPasswords = passwordObjects.reduce((valid, object) => {
+
+/**
+ * Part 1
+ */
+const validPasswordsPart1 = passwordObjects.reduce((valid, object) => {
 	const [min, max] = object.amount.split`-`
 	const letters = object.password.split``.filter(letter => letter === object.letter).length
 	letters >= min && letters <= max ? valid++ : null
 	return valid
 }, 0)
 
-console.log(validPasswords)
+console.log(validPasswordsPart1)
+
+/**
+ * Part 2
+ */
+const validPasswordsPart2 = passwordObjects.reduce((valid, object) => {
+	const [pos1, pos2] = object.amount.split`-`
+	const letters = object.password.split``	
+	const letter1 = letters[pos1]
+	const letter2 = letters[pos2]
+	
+	if(letter1 !== letter2 && (letter1 == object.letter || letter2 == object.letter))
+		valid++
+
+	return valid
+}, 0)
+
+console.log(validPasswordsPart2)
