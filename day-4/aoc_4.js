a231168b1fd4664e71de5e375ba20b5d52b71c7e
niQQe <nickewideving@gmail.com>
2020-12-05 18:57:32 +0100

    day-4

--- day-4/aoc_4.js
+++ day-4/aoc_4.js
@@ -8,8 +8,8 @@ const validFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 const validPassports = passports.reduce((fields, curr) => {
 	const fieldList = curr.reduce((acc, field) => {
-		const [ field ] = field.split`:`
-		if (field != 'cid') acc.push(field)
+		const [ firstField ] = field.split`:`
+		if (firstField != 'cid') acc.push(field)
 		return acc
 	}, [])
 	if (fieldList.length == validFields.length) fields++
