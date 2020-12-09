560657f66b92211e28607531a5bdc6d717ac5318
niQQe <nickewideving@gmail.com>
2020-12-09 21:47:01 +0100

    day-6 part2

--- day-6/aoc_6.js
+++ day-6/aoc_6.js
@@ -1,13 +1,33 @@
 const fs = require('fs')
 
-const declarations = fs.readFileSync('input.txt', 'utf-8').split(`\n\n`).map(v => v.replace(/[\n\r]/g, ''))
+const declarations = fs.readFileSync('input.txt', 'utf-8').split(`\n\n`)
 
 /**
  *  Part 1
  */
-const totalYesAnswers = declarations.reduce((yesAnswers, answer) => {
+
+const part1Declarations = declarations.map(v => v.replace(/[\n\r]/g, ''))
+
+const totalYesAnswers = part1Declarations.reduce((yesAnswers, answer) => {
 	yesAnswers += [...new Set(answer.split``)].length
 	return yesAnswers
 }, 0)
 
 console.log(totalYesAnswers)
+
+/**
+ * Part 2
+ */
+
+const part2Declarations = declarations.map(v => v.split`\n`)
+
+const onlyYesAnwers = part2Declarations.reduce((yesAnswers, group) => {
+	Object.keys(char = group.join``.split``.reduce((charAmount, amount) => {
+		charAmount[amount] = (charAmount[amount] || 0) + 1
+		return charAmount
+	}, {})).forEach(amount => char[amount] == group.length  ? yesAnswers++ : null )
+	return yesAnswers
+}, 0)
+
+console.log(onlyYesAnwers)
+
