efacc8b0f07742a8759390e0619fb2d32c4cd8d3
niQQe <nickewideving@gmail.com>
2020-12-17 16:10:00 +0100

    day-8 part2

--- day-8/aoc_8.js
+++ day-8/aoc_8.js
@@ -1,13 +1,16 @@
+
 const fs = require('fs')
 
 const instructions = fs.readFileSync('input.txt', 'utf-8')
 	.split`\n`
 	.map(v => v = { cmd: v.split` `[0], value: +v.split` `[1].replace(/\+/g, '') })
 
-
-let acc = 0
-let index = 0
+/**
+ * Part 1
+ */
 let visited = []
+let index = 0
+let acc = 0
 
 while(index < instructions.length){
 	if(visited.includes(index)) break;
@@ -28,3 +31,51 @@ while(index < instructions.length){
 }
 
 console.log(acc)
+
+/**
+ * 	Part 2
+ */
+const commandExecuter = (input) => {
+	let visited = []
+	let index = 0
+	let acc = 0
+	while (index < input.length) {
+		if (visited.includes(index)) return null
+		visited.push(index)
+		const { cmd, value } = input[index]
+		switch (cmd) {
+			case 'nop':
+				index++
+				break
+			case 'acc':
+				acc += value
+				index++
+				break
+			case 'jmp':
+				index += value
+				break
+		}
+	}
+	return acc
+}
+
+const part2Result = instructions.reduce((acc, _, i) => {
+	if (instructions[i].cmd == 'jmp') {
+		instructions[i].cmd = 'nop'
+
+		const result = commandExecuter(instructions)
+		if (result) acc = result
+
+		instructions[i].cmd = 'jmp'
+	} else if (instructions[i].cmd == 'nop') {
+		instructions[i].cmd = 'jmp'
+
+		const result = commandExecuter(instructions)
+		if (result) acc = result
+
+		instructions[i].cmd = 'nop'
+	}
+	return acc
+}, 0)
+
+console.log(part2Result)
