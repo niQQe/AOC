055bc689607555241107848c93993542dc294d1a
niQQe <nickewideving@gmail.com>
2020-12-19 21:29:23 +0100

    day-9 part2

--- day-9/aoc_9.js
+++ day-9/aoc_9.js
@@ -6,6 +6,7 @@ const range = (start, end) => {
 	return Array.from({ length: end - start }).map((_, i) => i + start);
 }
 
+
 const validNumbers = new Set(numbers.reduce((acc, _, i) => {
 	if (numbers[i + 25]) {
 		const arr = [...range(i, i + 25)].reduce((values, index) => {
@@ -30,6 +31,47 @@ const notFollowingRule = numbers.reduce((acc, n) => {
 	return acc
 }, 0)
 
-console.log(notFollowingRule)
 
+/**
+ *  Part 2
+ */
+
+
+const numbers2 = fs.readFileSync('input.txt', 'utf-8').split(`\n`).map(n => +n)
+
+let end = []
+
+function findEnd(arr) {
+	let sum = 0
+	for (let i = 0; i < arr.length; i++) {
+		sum += numbers[i]
+		if (sum > notFollowingRule) {
+			numbers.shift()
+			sum = 0
+			findEnd(numbers)
+		} else if (sum == notFollowingRule) {
+			if(end.length == 0)
+				end.push(numbers[i])
+		}
+	}
+}
+
+findEnd(numbers)
+
+let sum = 0
+
+let numArr = []
+
+for (var i = numbers2.indexOf(end[0]) + 1 - 1; i >= 0; i--) {
+	sum += numbers2[i]
+	numArr.push(numbers2[i])
+	if(sum == notFollowingRule)
+		break;
+}
+
+const [first, ...rest] = numArr.sort((a,b) => a-b)
+
+const result = first + rest[rest.length-1]
+
+console.log(result)
 
