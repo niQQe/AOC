f904d041fd1731c70d1d6975a0b002f95b759673
niQQe <nickewideving@gmail.com>
2020-12-26 00:02:57 +0100

    day-13 part1

--- day-13/aoc_13.js
+++ day-13/aoc_13.js
@@ -2,11 +2,12 @@
 
 const fs = require('fs')
 
-const [timestamp, ids] = fs.readFileSync('input.txt', 'utf-8').split(`\n`)
-const notes = { timestamp: +timestamp, ids: ids.replace(/([x])/g, '').split`,`.filter(v => v!= "").map(v => +v) }
+const [timestamp, rawIds] = fs.readFileSync('input.txt', 'utf-8').split(`\n`)
 
-const part1Result =  Math.min(...notes.ids.reduce((acc,id) => {
-	acc.push((Math.floor(((+timestamp + +Math.max(...notes.ids))/id)) * id - +timestamp) * id)
+const ids = rawIds.split `,`.filter(v=> +v).map(v => +v)
+
+const part1Result =  Math.min(...ids.reduce((acc,id) => {
+	acc.push((Math.floor(((+timestamp + Math.max(...ids))/id)) * id - +timestamp) * id)
 	return acc
 },[]))
 
