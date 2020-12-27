9cf350841bcf47cf72089eea70cc5beae2b51241
niQQe <nickewideving@gmail.com>
2020-12-27 01:06:55 +0100

    day-15 part1

---
+++ day-15/aoc_15.js
@@ -0,0 +1,35 @@
+let spokenHistory = [2,0,6,12,1,3,0]
+
+const target = 2020
+
+const isLastUnique = (n) => {
+	return !spokenHistory.slice(0, -1).includes(n)
+}
+
+const findTurnOfLastSpoken = (last) => {
+	const sortedIndexes = spokenHistory.reduce((acc, n, i) => {
+		if(n == last) acc.push(i)
+		return acc
+	},[]).sort((a,b) => b-a)
+
+	if(spokenHistory.slice(-2)[0] == last) return 1
+
+	if(sortedIndexes.length > 2) return sortedIndexes[0] - sortedIndexes[1]
+
+	const [largest, ...rest] = sortedIndexes
+
+	return largest - rest.slice(-1)[0]
+}
+
+for(let i = 0; i < spokenHistory.length; i++){
+	const last = spokenHistory.slice(-1)[0]
+
+	if(!isLastUnique(last)) spokenHistory.push(findTurnOfLastSpoken(last))
+	else spokenHistory.push(0)
+
+	if(spokenHistory.length == target) break
+}
+
+console.log(spokenHistory.slice(-1)[0])
+
+
