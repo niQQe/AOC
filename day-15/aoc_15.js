afefa68aaa6fc8602209672393612e418cc149f5
niQQe <nickewideving@gmail.com>
2020-12-27 01:11:01 +0100

    day-15 part1

--- day-15/aoc_15.js
+++ day-15/aoc_15.js
@@ -1,18 +1,18 @@
-let spokenHistory = [2, 0, 6, 12, 1, 3, 0]
+let turns = [2, 0, 6, 12, 1, 3, 0]
 
 const target = 2020
 
 const isLastUnique = (n) => {
-	return !spokenHistory.slice(0, -1).includes(n)
+	return !turns.slice(0, -1).includes(n)
 }
 
 const findTurnOfLastSpoken = (last) => {
-	const sortedIndexes = spokenHistory.reduce((acc, n, i) => {
+	const sortedIndexes = turns.reduce((acc, n, i) => {
 		if (n == last) acc.push(i)
 		return acc
 	}, []).sort((a, b) => b - a)
 
-	if (spokenHistory.slice(-2)[0] == last) return 1
+	if (turns.slice(-2)[0] == last) return 1
 
 	if (sortedIndexes.length > 2) return sortedIndexes[0] - sortedIndexes[1]
 
@@ -21,15 +21,15 @@ const findTurnOfLastSpoken = (last) => {
 	return largest - rest.slice(-1)[0]
 }
 
-while(spokenHistory.length != target) {
-	const last = spokenHistory.slice(-1)[0]
-	
+while(turns.length != target) {
+	const last = turns.slice(-1)[0]
+
 	if (!isLastUnique(last))
-		spokenHistory.push(findTurnOfLastSpoken(last))
+		turns.push(findTurnOfLastSpoken(last))
 	else 
-		spokenHistory.push(0)
+		turns.push(0)
 }
 
-console.log(spokenHistory.slice(-1)[0])
+console.log(turns.slice(-1)[0])
 
 
