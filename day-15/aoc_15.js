bbca2ea5a8489ebbee0204ce1609af8f94852165
niQQe <nickewideving@gmail.com>
2020-12-28 12:26:07 +0100

    day-15 part1

--- day-15/aoc_15.js
+++ day-15/aoc_15.js
@@ -1,23 +1,9 @@
 let turns = [2, 0, 6, 12, 1, 3]
 
-const findTurnOfLastSpoken = (last) => {
-	const sortedTurns = turns.reduce((acc, n, i) => {
-		if (n == last) acc.push(i)
-		return acc
-	}, []).sort((a, b) => b - a)
-
-	if (turns.slice(-2)[0] == last) return 1
-
-	if (sortedTurns.length > 2) return sortedTurns[0] - sortedTurns[1]
-
-	const [largest, ...rest] = sortedTurns
-
-	return largest - rest.slice(-1)[0]
-}
-
 while (turns.length != 2020) {
 	const last = turns.slice(-1)[0]
-	turns.slice(0, -1).includes(last) ? turns.push(findTurnOfLastSpoken(last)) : turns.push(0)
+	if (turns.slice(0, -1).includes(last)) turns.push(turns.length - 1 - turns.slice(0, -1).lastIndexOf(last))
+	else turns.push(0)
 }
 
 console.log(turns.slice(-1)[0])
